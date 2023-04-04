f108f0ed0e011f04b0a970a9e596d8f958bb3a72
Brice <brice.verriere@epitech.eu>
2023-04-04 16:59:35 +0200

    props passés

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,23 @@ import './App.css'
 import MyTitle from "./components/MyTitle"
 import PokemonCard from './components/PokemonCard';
 
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function App() {
   return (
     <div>
       <MyTitle />
       <div>
-        <PokemonCard />
+        <PokemonCard pokemon = {pokemonList[0]}/>
       </div>
     </div>
   );
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,21 +1,10 @@
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
 
-function PokemonCard() {
-    const pokemon = pokemonList[0];
-    //console.log(pokemon)
+
+function PokemonCard(props) {
     return (
         <div>
-            <h2>{pokemon.name}</h2>
-            {pokemon.Src = "undefined" ? <img src={pokemon.imgSrc}></img> : <p></p>}
+            <h2>{props.pokemon.name}</h2>
+            {props.pokemon.imgSrc ? <img src={props.pokemon.imgSrc}></img> : <p></p>}
         </div>
     );
 }
