12e3cdbc8c0d3cdcbd3c90d11b9597378ec53e3e
Brice <brice.verriere@epitech.eu>
2023-04-05 09:54:48 +0200

    ajout state pokemonIndex + boutons suivant et précédent

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,52 @@ import './App.css'
 import MyTitle from "./components/MyTitle"
 import PokemonCard from './components/PokemonCard';
 
+
+
 const pokemonList = [
   {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
 function App() {
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+  const handleClickPlus = () => {
+    setPokemonIndex(pokemonIndex + 1)
+    //console.log(pokemonIndex)
+  }
+  const handleClickMoins = () => {
+    setPokemonIndex(pokemonIndex - 1)
+    //console.log(pokemonIndex)
+  }
   return (
     <div>
       <MyTitle />
       <div>
-        <PokemonCard pokemon = {pokemonList[0]}/>
+        <PokemonCard pokemon = {pokemonList[pokemonIndex]}/>
       </div>
+      {pokemonIndex > 0 ?<button onClick={handleClickMoins}>Précédent</button> : <></>}
+      {pokemonIndex < pokemonList.length - 1 ?<button onClick={handleClickPlus}>Suivant</button> : <></>}
     </div>
   );
 }
